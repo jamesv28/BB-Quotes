af20e569c6fef4220c407941e8c2f1b5e409d065
James Volmert <james.volmert@gmail.com>
2024-10-09 16:59:01 -0600

    finished services for view model

---
+++ BB-Quotes/ViewModel/ViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ViewModel.swift
+//  BB-Quotes
+//
+//  Created by James Volmert on 10/8/24.
+//
+
+import Foundation
+
+@Observable
+class ViewModel {
+    enum FetchStatus {
+        case notStarted
+        case fetching
+        case success
+        case failed(error: Error)
+    }
+    private(set) var status: FetchStatus = .notStarted
+    
+    private let fetcher = FetchService()
+    
+    var quote: Quote
+    var character: Character
+    
+    init() {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let quoteData = try! Data(contentsOf: Bundle.main.url(forResource: "samplequote", withExtension: "json")!)
+        quote = try! decoder.decode(Quote.self, from: quoteData)
+        
+        let characterData = try! Data(contentsOf: Bundle.main.url(forResource: "samplecharacter", withExtension: "json")!)
+        character = try! decoder.decode(Character.self, from: characterData)
+    }
+    
+    func getData(for show: String) async {
+        status = .fetching
+        
+        do {
+            quote = try await fetcher.fetchQuote(from: show)
+            character = try await fetcher.fetchCharacter(quote.charater)
+            character.death = try await fetcher.fetchDeaths(for: character.name)
+            
+            status = .success
+        } catch {
+            status = .failed(error: error)
+        }
+    }
+}
